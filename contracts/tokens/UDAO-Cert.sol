847999f73d71682d9930526d6e8ef6e302c8d1a8
batuhandrcn <batuhan.darcin@anywai.ch>
2023-11-20 12:12:42 +0300

    fix(UdaoCert): removed unused requirement from emergencyTransfer
    Co-authored-by: Bugrahan Duran <bugrahan.duran@anywai.ch>

--- contracts/tokens/UDAO-Cert.sol
+++ contracts/tokens/UDAO-Cert.sol
@@ -133,7 +133,7 @@ contract UDAOCertificate is
         return ECDSA.recover(digest, voucher.signature);
     }
 
-    /// @notice Checks if token transfer is allowed. 
+    /// @notice Checks if token transfer is allowed.
     /// @param from The current token owner
     /// @param to Token to send to
     /// @param tokenId The id of the token to transfer
@@ -165,13 +165,7 @@ contract UDAOCertificate is
         address to,
         uint256 tokenId
     ) external {
-        require(
-            hasRole(BACKEND_ROLE, msg.sender),
-            "You don't have right to transfer token"
-        );
-        //make sure "to" address is kyced and not banned
-        require(isKYCed(to, 12), "Receiver is not KYCed");
-        require(isNotBanned(to, 12), "Receiver is banned");
+        /// @dev only backend can transfer token in emergency (in beforeTokenTransfer)
         _transfer(from, to, tokenId);
     }
 
